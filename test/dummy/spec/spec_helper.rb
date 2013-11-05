ff6f23e458f58ef1baaa9400a99be0b7bfa0aa20
Jeremy Fairbank <elpapapollo@gmail.com>
2013-11-05 16:02:23 -0500

    Add 'FactoryGirl' methods to specs by including 'FactoryGirl::Syntax::Methods'

--- test/dummy/spec/spec_helper.rb
+++ test/dummy/spec/spec_helper.rb
@@ -39,6 +39,9 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # FactoryGirl configuration
+  config.include FactoryGirl::Syntax::Methods
 end
 
 def clear_user
