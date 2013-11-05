d0c92b42a501b191128d50acd456473951a1795f
Jeremy Fairbank <elpapapollo@gmail.com>
2013-11-05 16:34:40 -0500

    Rename file to 'user_enum_spec.rb' and switch 'editor' and 'author' roles positioning in tests and setup

--- test/dummy/spec/models/user_enum_spec.rb
+++ test/dummy/spec/models/user_enum_spec.rb
@@ -10,57 +10,52 @@ describe User do
   shared_examples 'it sets default values' do
     it 'sets the default values' do
       expect(User::Role::ADMIN).to eq(0)
-      expect(User::Role::AUTHOR).to eq(1)
-      expect(User::Role::EDITOR).to eq(2)
+      expect(User::Role::EDITOR).to eq(1)
+      expect(User::Role::AUTHOR).to eq(2)
       expect(User::Role::USER).to eq(3)
     end
 
     it 'sets the right value for an instance' do
-      expect(u_admin.role).to eq(0)
-      expect(u_author.role).to eq(1)
-      expect(u_editor.role).to eq(2)
-      expect(u_user.role).to eq(3)
+      expect(admin.role).to eq(0)
+      expect(editor.role).to eq(1)
+      expect(author.role).to eq(2)
+      expect(user.role).to eq(3)
     end
   end
 
   shared_examples 'it sets default labels' do
     it 'sets the default labels' do
       expect(User::Role::ADMIN_LABEL).to eq('Admin')
-      expect(User::Role::AUTHOR_LABEL).to eq('Author')
       expect(User::Role::EDITOR_LABEL).to eq('Editor')
+      expect(User::Role::AUTHOR_LABEL).to eq('Author')
       expect(User::Role::USER_LABEL).to eq('User')
     end
   end
 
-  shared_examples 'it sets the appropriate values' do |admin_value, author_value, editor_value, user_value|
+  shared_examples 'it sets the appropriate values' do |admin_value, editor_value, author_value, user_value|
     it 'sets the appropriate values' do
       expect(User::Role::ADMIN).to eq(admin_value)
-      expect(User::Role::AUTHOR).to eq(author_value)
       expect(User::Role::EDITOR).to eq(editor_value)
+      expect(User::Role::AUTHOR).to eq(author_value)
       expect(User::Role::USER).to eq(user_value)
     end
 
     it 'sets the right value for an instance' do
-      expect(u_admin.role).to eq(admin_value)
-      expect(u_author.role).to eq(author_value)
-      expect(u_editor.role).to eq(editor_value)
-      expect(u_user.role).to eq(user_value)
+      expect(admin.role).to eq(admin_value)
+      expect(author.role).to eq(author_value)
+      expect(editor.role).to eq(editor_value)
+      expect(user.role).to eq(user_value)
     end
   end
 
-  let(:u_admin)  { User.new(role: User::Role::ADMIN) }
-  let(:u_author) { User.new(role: User::Role::AUTHOR) }
-  let(:u_editor) { User.new(role: User::Role::EDITOR) }
-  let(:u_user)   { User.new(role: User::Role::USER) }
+  let(:admin)  { User.new(role: User::Role::ADMIN) }
+  let(:editor) { User.new(role: User::Role::EDITOR) }
+  let(:author) { User.new(role: User::Role::AUTHOR) }
+  let(:user)   { User.new(role: User::Role::USER) }
 
   context 'when passing all symbols' do
     before :each do
-      clear_user
-
-      User.class_eval do
-        extend RailsAttrEnum
-        attr_enum :role, :admin, :author, :editor, :user
-      end
+      default_user_roles
     end
 
     it_behaves_like 'it has the enum module'
@@ -75,7 +70,7 @@ describe User do
 
         User.class_eval do
           extend RailsAttrEnum
-          attr_enum :role, :admin, { author: 12 }, :editor, { user: 42 }
+          attr_enum :role, :admin, { editor: 12 }, :author, { user: 42 }
         end
       end
 
@@ -90,7 +85,7 @@ describe User do
 
         User.class_eval do
           extend RailsAttrEnum
-          attr_enum :role, admin: 1, author: 2, editor: 4, user: 8
+          attr_enum :role, admin: 1, editor: 2, author: 4, user: 8
         end
       end
 
