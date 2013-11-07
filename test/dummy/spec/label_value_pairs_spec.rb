e7debe4b55b8d7cb261ede7b4392d6dcd3fb2ae3
Jeremy Fairbank <elpapapollo@gmail.com>
2013-11-07 11:39:59 -0500

    Initial commit

---
+++ test/dummy/spec/label_value_pairs_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_Helper'
+
+describe 'User::Role.label_value_pairs' do
+  let(:enum) { User::Role }
+
+  before { default_user_roles }
+
+  let :all_pairs do
+    [[enum::ADMIN_LABEL,  enum::ADMIN],
+     [enum::EDITOR_LABEL, enum::EDITOR],
+     [enum::AUTHOR_LABEL, enum::AUTHOR],
+     [enum::USER_LABEL,   enum::USER]]
+  end
+
+  context 'with no arguments' do
+    subject { enum.label_value_pairs }
+
+    it { should have(4).items }
+    it { should eq(all_pairs) }
+  end
+
+  context 'with one argument' do
+    subject { enum.label_value_pairs(:editor) }
+
+    it { should have(1).item }
+    it { should eq([[enum::EDITOR_LABEL, enum::EDITOR]]) }
+  end
+
+  context 'with more than one argument' do
+    subject { enum.label_value_pairs(:author, :editor, :admin) }
+
+    it { should have(3).items }
+
+    it {
+      should eq [[enum::ADMIN_LABEL,  enum::ADMIN],
+                 [enum::EDITOR_LABEL, enum::EDITOR],
+                 [enum::AUTHOR_LABEL, enum::AUTHOR]]
+    }
+  end
+end
---
+++ test/dummy/spec/mapped_entries_methods_spec.rb
@@ -0,0 +1,25 @@
+describe 'User::Role' do
+  context 'with mapped entries method' do
+    let(:enum) { User::Role }
+
+    before { default_user_roles }
+
+    describe '.keys' do
+      it 'returns all the keys' do
+        expect(enum.keys).to eq [:admin, :editor, :author, :user]
+      end
+    end
+
+    describe '.values' do
+      it 'returns all the values' do
+        expect(enum.values).to eq [0, 1, 2, 3]
+      end
+    end
+
+    describe '.labels' do
+      it 'returns all the labels' do
+        expect(enum.labels).to eq %w(Admin Editor Author User)
+      end
+    end
+  end
+end
